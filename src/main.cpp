9f8e62c62cee52f4f9df539a58a31208c754a36a
Oliver <otlg123@gmail.com>
2020-12-27 00:17:29 +0000

    Start implementing NV storage for the SSID/password

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+
+#define USE_DASH
+#ifdef USE_DASH
 #include <ESPDash.h>
+#endif
+
 #include <ESPmDNS.h>
 #include <Button2.h>
 #include <Wire.h>
@@ -13,13 +18,158 @@
 #include <Adafruit_BME280.h>
 #include <WiFiMulti.h>
 #include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "nvs.h"
 
 #define SOFTAP_MODE
 // #define USE_18B20_TEMP_SENSOR
 // #define USE_CHINESE_WEB
 
+void initNVS()
+{
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    if (err != ESP_OK)
+    {
+        Serial.println("NVS error: ");
+        Serial.println(err);
+    }
+    else
+    {
+        Serial.println("NVS OK");
+    }
+}
 
+#define MAX_SSID_LENGTH 20
+#define MAX_PWD_LENGTH 30
 
+typedef uint32_t nvs_handle_t;
+
+/// @param[out] ssid should be an char array of MAX_SSID_LENGTH elements
+/// @param[out] pwd should be an char array of MAX_PWD_LENGTH elements 
+/// returns true if values were loaded from memory, false otherwise
+bool tryReadSSIDPW(char *ssid, char *pwd)
+{
+    Serial.println("tryReadSSIDPW");
+    nvs_handle_t storage;
+    esp_err_t err = nvs_open("store", NVS_READWRITE, &storage);
+    if (err != ESP_OK)
+    {
+        Serial.print("nvs_open: ");
+        Serial.println(esp_err_to_name(err));
+        return false;
+    }
+    else
+    {
+        // Read ssid 
+        size_t length = MAX_SSID_LENGTH;
+        err = nvs_get_str(storage, "ssid", ssid, &length);
+        switch (err)
+        {
+        case ESP_OK:
+            Serial.print("SSID was: ");
+            Serial.println(ssid);
+            break;
+        default:
+            Serial.print("nvs_get_str(ssid): ");
+            Serial.println(esp_err_to_name(err));
+            nvs_close(storage);
+            return false;
+        }
+
+        if (length > MAX_SSID_LENGTH)
+        {
+            Serial.println("Stored SSID too long");
+            nvs_close(storage);
+            return false;
+        }
+
+        // read pwd
+        length = MAX_PWD_LENGTH;
+        err = nvs_get_str(storage, "pwd", pwd, &length);
+        switch (err)
+        {
+        case ESP_OK:
+            Serial.print("pwd was: ");
+            Serial.println(pwd);
+            break;
+        default:
+            Serial.print("nvs_get_str(pwd): ");
+            Serial.println(esp_err_to_name(err));
+            nvs_close(storage);
+            return false;
+        }
+
+        if (length > MAX_PWD_LENGTH)
+        {
+            Serial.println("Stored PWD too long");
+            nvs_close(storage);
+            return false;
+        }
+
+        return true;
+    }
+}
+
+/// @param[in] ssid should be an char array of maximum MAX_SSID_LENGTH elements
+/// @param[in] pwd should be an char array of maximum MAX_PWD_LENGTH elements 
+/// returns true if values were written to memory, false otherwise
+bool writeSSIDPW(char *ssid, char *pwd)
+{
+    Serial.println("writeSSIDPW");
+    nvs_handle_t storage;
+    esp_err_t err = nvs_open("store", NVS_READWRITE, &storage);
+    if (err != ESP_OK)
+    {
+        Serial.print("nvs_open: ");
+        Serial.println(esp_err_to_name(err));
+        return false;
+    }
+    else
+    {
+        // Write SSID
+        err = nvs_set_str(storage, "ssid", ssid);
+        if (err != ESP_OK)
+        {
+            Serial.print("nvs_set_str(ssid): ");
+            Serial.println(esp_err_to_name(err));
+            nvs_close(storage);
+            return false;
+        }
+
+        // write password
+        err = nvs_set_str(storage, "pwd", pwd);
+        if (err != ESP_OK)
+        {
+            Serial.print("nvs_set_str(pwd): ");
+            Serial.println(esp_err_to_name(err));
+            nvs_close(storage);
+            return false;
+        }
+
+        // commit
+        err = nvs_commit(storage);
+        if (err != ESP_OK)
+        {
+            Serial.print("nvs_commit: ");
+            Serial.println(esp_err_to_name(err));
+            nvs_close(storage);
+            return false;
+        }
+
+        nvs_close(storage);
+        return true;
+    }
+}
+
+#ifdef USE_18B20_TEMP_SENSOR
 // Simple ds18b20 class
 class DS18B20
 {
@@ -32,7 +182,8 @@ public:
     float temp()
     {
         uint8_t arr[2] = {0};
-        if (reset()) {
+        if (reset())
+        {
             wByte(0xCC);
             wByte(0x44);
             delay(750);
@@ -46,6 +197,7 @@ public:
         }
         return 0;
     }
+
 private:
     int pin;
 
@@ -54,7 +206,8 @@ private:
         pinMode(pin, OUTPUT);
         digitalWrite(pin, LOW);
         delayMicroseconds(5);
-        if (bit)digitalWrite(pin, HIGH);
+        if (bit)
+            digitalWrite(pin, HIGH);
         delayMicroseconds(80);
         digitalWrite(pin, HIGH);
     }
@@ -72,7 +225,8 @@ private:
 
     void wByte(uint8_t bytes)
     {
-        for (int i = 0; i < 8; ++i) {
+        for (int i = 0; i < 8; ++i)
+        {
             write((bytes >> i) & 1);
         }
         delayMicroseconds(100);
@@ -81,8 +235,10 @@ private:
     uint8_t rByte()
     {
         uint8_t r = 0;
-        for (int i = 0; i < 8; ++i) {
-            if (read()) r |= 1 << i;
+        for (int i = 0; i < 8; ++i)
+        {
+            if (read())
+                r |= 1 << i;
             delayMicroseconds(15);
         }
         return r;
@@ -99,20 +255,18 @@ private:
         return digitalRead(pin);
     }
 };
+#endif
 
-
-
-#define I2C_SDA             25
-#define I2C_SCL             26
-#define DHT12_PIN           16
-#define BAT_ADC             33
-#define SALT_PIN            34
-#define SOIL_PIN            32
-#define BOOT_PIN            0
-#define POWER_CTRL          4
-#define USER_BUTTON         35
-#define DS18B20_PIN         21                  //18b20 data pin
-
+#define I2C_SDA 25
+#define I2C_SCL 26
+#define DHT12_PIN 16
+#define BAT_ADC 33
+#define SALT_PIN 34
+#define SOIL_PIN 32
+#define BOOT_PIN 0
+#define POWER_CTRL 4
+#define USER_BUTTON 35
+#define DS18B20_PIN 21 //18b20 data pin
 
 BH1750 lightMeter(0x23); //0x23
 Adafruit_BME280 bmp;     //0x77
@@ -121,12 +275,12 @@ AsyncWebServer server(80);
 Button2 button(BOOT_PIN);
 Button2 useButton(USER_BUTTON);
 WiFiMulti multi;
+#ifdef USE_18B20_TEMP_SENSOR
 DS18B20 temp18B20(DS18B20_PIN);
+#endif
 
-#define WIFI_SSID   "your wifi ssid"
+#define WIFI_SSID "your wifi ssid"
 #define WIFI_PASSWD "you wifi password"
-
-
 
 bool bme_found = false;
 
@@ -135,7 +289,8 @@ void smartConfigStart(Button2 &b)
     Serial.println("smartConfigStart...");
     WiFi.disconnect();
     WiFi.beginSmartConfig();
-    while (!WiFi.smartConfigDone()) {
+    while (!WiFi.smartConfigDone())
+    {
         Serial.print(".");
         delay(200);
     }
@@ -155,22 +310,27 @@ void sleepHandler(Button2 &b)
     esp_deep_sleep_start();
 }
 
-
 bool serverBegin()
 {
     static bool isBegin = false;
-    if (isBegin) {
+    if (isBegin)
+    {
         return true;
     }
 
+#ifdef USE_DASH
     ESPDash.init(server);
+#endif
 
     isBegin = true;
-    if (MDNS.begin("soil")) {
+    if (MDNS.begin("soil"))
+    {
         Serial.println("MDNS responder started");
     }
     // Add Respective Cards
-    if (bme_found) {
+#ifdef USE_DASH
+    if (bme_found)
+    {
 #ifdef USE_CHINESE_WEB
         ESPDash.addTemperatureCard("temp", "BME传感器温度/C", 0, 0);
         ESPDash.addNumberCard("press", "BME传感器压力/hPa", 0);
@@ -197,19 +357,24 @@ bool serverBegin()
     ESPDash.addNumberCard("batt", "Battery/mV", 0);
 #endif
 
-
 #ifdef USE_18B20_TEMP_SENSOR
     ESPDash.addTemperatureCard("temp3", "18B20温度/C", 0, 0);
 #endif
     ESPDash.addTemperatureCard("temp3", "18B20 Temperature/C", 0, 0);
+#endif
     server.begin();
     MDNS.addService("http", "tcp", 80);
     return true;
 }
 
+char ssid[MAX_SSID_LENGTH];
+char pwd[MAX_PWD_LENGTH];
+
 void setup()
 {
     Serial.begin(115200);
+
+    initNVS();
 
 #ifdef SOFTAP_MODE
     Serial.println("Configuring access point...");
@@ -218,25 +383,51 @@ void setup()
     esp_wifi_get_mac(WIFI_IF_AP, mac);
     sprintf(buff, "T-Higrow-%02X:%02X", mac[4], mac[5]);
     WiFi.softAP(buff);
+    Serial.println(buff);
+
+    char ssidT[6] = "abcd";
+    char pwdT[6] = "1234";
+    if (!writeSSIDPW(ssidT, pwdT))
+    {
+        Serial.println("Couldn't write details");
+    }
+
+    memset(ssid, 0, MAX_SSID_LENGTH);
+    memset(pwd, 0, MAX_PWD_LENGTH);
+    if (!tryReadSSIDPW(ssid, pwd))
+    {
+        Serial.println("No saved details");
+    }
+    Serial.print("SSID: ");
+    Serial.println(ssid);
+    
+    Serial.print("PWD: ");
+    Serial.println(pwd);
 #else
     WiFi.mode(WIFI_STA);
     wifi_config_t current_conf;
     esp_wifi_get_config(WIFI_IF_STA, &current_conf);
     int ssidlen = strlen((char *)(current_conf.sta.ssid));
     int passlen = strlen((char *)(current_conf.sta.password));
-    if (ssidlen == 0 || passlen == 0) {
+    if (ssidlen == 0 || passlen == 0)
+    {
         multi.addAP(WIFI_SSID, WIFI_PASSWD);
         Serial.println("Connect to defalut ssid, you can long press BOOT button enter smart config mode");
-        while (multi.run() != WL_CONNECTED) {
+        while (multi.run() != WL_CONNECTED)
+        {
             Serial.print('.');
         }
-    } else {
+    }
+    else
+    {
         WiFi.begin();
     }
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
         Serial.printf("WiFi connect fail!,please restart retry,or long press BOOT button enter smart config mode\n");
     }
-    if (WiFi.status() == WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
         Serial.print("IP Address: ");
         Serial.println(WiFi.localIP());
     }
@@ -253,22 +444,25 @@ void setup()
     digitalWrite(POWER_CTRL, 1);
     delay(1000);
 
-    if (!bmp.begin()) {
+    if (!bmp.begin())
+    {
         Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
         bme_found = false;
-    } else {
+    }
+    else
+    {
         bme_found = true;
     }
 
-    if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+    if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE))
+    {
         Serial.println(F("BH1750 Advanced begin"));
-    } else {
+    }
+    else
+    {
         Serial.println(F("Error initialising BH1750"));
     }
-
-
 }
-
 
 uint32_t readSalt()
 {
@@ -276,13 +470,16 @@ uint32_t readSalt()
     uint32_t humi = 0;
     uint16_t array[120];
 
-    for (int i = 0; i < samples; i++) {
+    for (int i = 0; i < samples; i++)
+    {
         array[i] = analogRead(SALT_PIN);
         delay(2);
     }
     std::sort(array, array + samples);
-    for (int i = 0; i < samples; i++) {
-        if (i == 0 || i == samples - 1)continue;
+    for (int i = 0; i < samples; i++)
+    {
+        if (i == 0 || i == samples - 1)
+            continue;
         humi += array[i];
     }
     humi /= samples - 2;
@@ -308,38 +505,54 @@ void loop()
     static uint64_t timestamp;
     button.loop();
     useButton.loop();
-    if (millis() - timestamp > 1000 ) {
+    if (millis() - timestamp > 1000)
+    {
         timestamp = millis();
         // if (WiFi.status() == WL_CONNECTED) {
-        if (serverBegin()) {
+        if (serverBegin())
+        {
             float lux = lightMeter.readLightLevel();
 
-            if (bme_found) {
+            if (bme_found)
+            {
                 float bme_temp = bmp.readTemperature();
                 float bme_pressure = (bmp.readPressure() / 100.0F);
                 float bme_altitude = bmp.readAltitude(1013.25);
+#ifdef USE_DASH
                 ESPDash.updateTemperatureCard("temp", (int)bme_temp);
                 ESPDash.updateNumberCard("press", (int)bme_pressure);
                 ESPDash.updateNumberCard("alt", (int)bme_altitude);
+#endif
             }
 
             float t12 = dht12.readTemperature();
             // Read temperature as Fahrenheit (isFahrenheit = true)
             float h12 = dht12.readHumidity();
 
-
-            if (!isnan(t12) && !isnan(h12) ) {
+#ifdef USE_DASH
+            if (!isnan(t12) && !isnan(h12))
+            {
                 ESPDash.updateTemperatureCard("temp2", (int)t12);
                 ESPDash.updateHumidityCard("hum2", (int)h12);
             }
             ESPDash.updateNumberCard("lux", (int)lux);
+#endif
 
             uint16_t soil = readSoil();
             uint32_t salt = readSalt();
             float bat = readBattery();
+#ifdef USE_DASH
             ESPDash.updateHumidityCard("soil", (int)soil);
             ESPDash.updateNumberCard("salt", (int)salt);
             ESPDash.updateNumberCard("batt", (int)bat);
+#else
+            Serial.print("soil ");
+            Serial.println(soil);
+            Serial.print("salt ");
+            Serial.println(salt);
+            Serial.print("batt ");
+            Serial.println(bat);
+#endif
 
 #ifdef USE_18B20_TEMP_SENSOR
             //Single data stream upload
